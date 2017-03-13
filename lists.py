f44842e63e1e2f1ad8364cf290915557a584ebf4
santhoshkumar vaddi <santhoshkumar vaddi>
2017-03-12 20:03:12 -0400

    ckecking

--- lists.py
+++ lists.py
@@ -3,8 +3,10 @@ names=['Adarsh','munna']
 myList=[0,1,2,3,4,5,6,7,8,9]
 
 
-myList=[2,3,1,3,54,4,6,22]
+myList=[2,3,1,3,54,4,6,22,60]
 
 input("Press any key to exit")
 
 print ("new to git ")
+
+print ("checking")
